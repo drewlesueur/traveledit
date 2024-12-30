05e75b4c1e4bebebdc14a0d1ae601545145db647
Drew LeSueur <drewalex@gmail.com>
2024-12-29 21:39:48 -0700

    linescript wip

---
+++ src/linescript/example.ls
@@ -0,0 +1,8 @@
+say "hello world"
+
+# how is everyone
+# 
+# ok (yo man)
+# 
+# smthng"
+# yo "wrld" yo"smthng
---
+++ src/linescript/linescript.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+func popVal(state map[string]any) any {
+    valStack := state["__valStack"].([]any)
+    poppedValue := valStack[len(valStack)-1]
+    state["__valStack"] = valStack[:len(valStack)-1]
+    return poppedValue
+}
+func get(state map[string]any, field string) any {
+    // TODO: parse out dot
+    if field[0] == '\'' {
+        return field[1:]
+    }
+    return state[field[1:]]
+}
+
+var builtins = map[string]func(state map[string]any) {
+    "get": func(state map[string]any) {
+    },
+    "say": func(state map[string]any) {
+        val := get(state, popVal(state).(string))
+        fmt.Println(val)
+    },
+}
+
+func callFunc(state map[string]any) {
+    
+}
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Please provide a file name.")
+        return
+    }
+
+    fileName := os.Args[1]
+    data, err := os.ReadFile(fileName)
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+    fmt.Println(string(data))
+    code := string(data)
+
+    i := 0
+    token := ""
+    state := map[string]any{
+        "__valStack": []any{},
+        "__i": 0,
+        "__inCall": false,
+        "__argCount": 0,
+        "__currFuncName": "",
+    }
+
+    for j := 0; j < 1000; j++ {
+        token, i = nextToken(code, i)
+        state["__i"] = i
+        if token == "" {
+            break
+        }
+        fmt.Printf("token: %q\n", token)
+        fmt.Printf("i: %d\n", i)
+        continue
+        if token == "\n" {
+            callFunc(state)
+            state["__inCall"] = false
+            state["__argCount"] = 0
+            continue
+        }
+        if state["__inCall"].(bool) {
+            state["__inCall"] = true
+            state["__curFuncName"] = token
+        } else {
+            state["__valStack"] = append(state["__valStack"].([]any), token)
+            state["__argCount"] = state["__argCount"].(int) + 1
+        }
+    }
+}
+
+func nextToken(code string, i int) (string, int) {
+    if i > len(code) {
+        return "", -1
+    }
+    state := "out" // "string", "in"
+    start := -1
+    for i = i; i < len(code); i++ {
+        b := code[i]
+        switch state {
+        case "out":
+            switch b {
+            case '{', '}', '(', ')', '[', ']':
+                return string(b), i+1
+            case ',', '\n':
+                return "\n", i+1
+            case ' ', '\t', '\r':
+                continue
+            case '"', '\'':
+                expectedQuoteEnd := string(code[i])
+                end := strings.Index(code[i+1:], expectedQuoteEnd)
+                return "'" + code[i+1:i+1+end], i+1+end+1
+            case '#':
+                // comments
+                end := strings.Index(code[i+1:], "\n")
+                if end == -1 {
+                    return "", -1
+                }
+                i = i+1+end
+            default:
+                state = "in"
+                start = i
+            }
+        case "in":
+            switch b {
+            case '{', '}', '(', ')', '[', ']':
+                return "$" + code[start:i], i
+            case ',', '\n':
+                return "$" + code[start:i], i
+            case ' ', '\t', '\r':
+                return "$" + code[start:i], i+1
+            case '"', '\'':
+                expectedQuoteEnd := string(code[i]) + code[start:i-1]
+                endIndex := strings.Index(code[i+1:], code[start:i])
+                token := code[i+1:i+1+endIndex]
+                return "'" + token, i + 1 + endIndex + len(expectedQuoteEnd) + 1
+            default:
+            }
+        }
+    }
+    if state == "in" {
+        return "$" + code[start:i], i+1
+    }
+    return "", -1
+}
