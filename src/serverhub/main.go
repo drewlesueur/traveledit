d6ec0c7e68b8338784ebb7198972cb4c99aa2415
Drew LeSueur <drewalex@gmail.com>
2025-02-09 22:43:05 -0700

    wip updates

--- src/serverhub/main.go
+++ src/serverhub/main.go
@@ -1,4 +1,3 @@
-package main
 
 // allow to proxy
 // allow to run any number of scripts
@@ -20,115 +19,37 @@ hub can create, update, delete files on server
     run shell scripts
 and then run cgi scripts
 inter-process locking?
+
+
+It can proxy by hostname or a path.
+It can keep or clear the path.
+It has "satellites" that can sit on other servers, poll it for requests and either respond, or also proxy to local http server.
+cgi?! (edited)
+it can handle all things ssh?!
+what about performance, extra hops? the satellites can run arbitrary scripts statellites have a cgi-bin directory and anything there can be run as cgi
+all http for now
+redis model where you run scripts against an existing server (aka already running program)
+idea start with just a cgi server implement the whole thing in linescript with just file locks and filesystem. Then add more perf later
+
+fastcgi?
+
+
 */
 
+package main
+
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
-	"log"
-	"net/http"
-	"sync"
-	"time"
-)
-
-type CertificateReloader struct {
-	mu        sync.RWMutex
-	certs     map[string]*tls.Certificate // Map of hostnames to certificates
-}
-
-func (r *CertificateReloader) GetCertificate(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
-	if cert, ok := r.certs[clientHello.ServerName]; ok {
-		return cert, nil
-	}
-	return nil, fmt.Errorf("no certificate for host: %s", clientHello.ServerName)
-}
-
-func (r *CertificateReloader) LoadCertificate(hostname string, certFile, keyFile string) error {
-	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return fmt.Errorf("could not load certificate for host %s: %v", hostname, err)
-	}
-
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if r.certs == nil {
-		r.certs = make(map[string]*tls.Certificate)
-	}
-	r.certs[hostname] = &cert
-	return nil
-}
-
-func main() {
-	reloader := &CertificateReloader{}
-
-	hostCerts := map[string][2]string{
-		"example.com": {"example.com.crt", "example.com.key"},
-		"another.com": {"another.com.crt", "another.com.key"},
-	}
-
-	for host, files := range hostCerts {
-		if err := reloader.LoadCertificate(host, files[0], files[1]); err != nil {
-			log.Fatalf("initial certificate load failed for %s: %v", host, err)
-		}
-	}
-
-	// Periodically reload the certificates
-	go func() {
-		for {
-			time.Sleep(10 * time.Second)
-			for host, files := range hostCerts {
-				if err := reloader.LoadCertificate(host, files[0], files[1]); err != nil {
-					log.Printf("failed to reload certificate for %s: %v", host, err)
-				} else {
-					log.Printf("certificate reloaded successfully for %s", host)
-				}
-			}
-		}
-	}()
-
-	server := &http.Server{
-		Addr: ":443",
-		TLSConfig: &tls.Config{
-			GetCertificate: reloader.GetCertificate,
-		},
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Hello, HTTPS!")
-		}),
-	}
-
-	log.Println("Starting HTTPS server on :443")
-	log.Fatal(server.ListenAndServeTLS("", "")) // Certificates are loaded dynamically, based on the hostname
-}
-
-update this so every time it gets a request
-it looks in the filesystem for the certs
-for example if foo.example.com it hit
-
-it will look for ./certs/foo.example.com/privkey.pem and ./certs/foo.example.com/fullchain.pem
-have it do it on the fly but cache the results
-
-also have an endpoint that will clear the cache to force a reload next time
-
-To update the code to dynamically load certificates from the filesystem on each request and cache the results, you can implement the following changes:
-
-1. Modify the `GetCertificate` method to attempt to load the certificate from the filesystem if it is not already in the cache.
-2. Implement a method to clear the cache, which can be triggered by an endpoint.
-
-Here's how you can modify the code:
-
-```go
-import (
-	"crypto/tls"
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"sync"
-	"time"
 )
 
 type CertificateReloader struct {
@@ -137,74 +58,73 @@ type CertificateReloader struct {
 }
 
 func formatWildcardDomain(domain string) string {
-    parts := strings.SplitN(domain, ".", 2)
-    if len(parts) != 2 {
-        return ""
-    }
-    return "_." + parts[1]
+	parts := strings.SplitN(domain, ".", 2)
+	if len(parts) != 2 {
+		return ""
+	}
+	return "_." + parts[1]
 }
 
 func (r *CertificateReloader) getWildcardCertificate(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-    wildcardDomain := formatWildcardDomain(clientHello.ServerName)
-    if wildcardDomain == "" {
-        return nil, fmt.Errorf("invalid server name: %s", clientHello.ServerName)
-    }
+	wildcardDomain := formatWildcardDomain(clientHello.ServerName)
+	if wildcardDomain == "" {
+		return nil, fmt.Errorf("invalid server name: %s", clientHello.ServerName)
+	}
 
-    r.mu.RLock()
-    cert, ok := r.certs[wildcardDomain]
-    r.mu.RUnlock()
-    if ok {
-        return cert, nil
-    }
+	r.mu.RLock()
+	cert, ok := r.certs[wildcardDomain]
+	r.mu.RUnlock()
+	if ok {
+		return cert, nil
+	}
 
-    certFile := filepath.Join("certs", wildcardDomain, "fullchain.pem")
-    keyFile := filepath.Join("certs", wildcardDomain, "privkey.pem")
+	certFile := filepath.Join("certs", wildcardDomain, "fullchain.pem")
+	keyFile := filepath.Join("certs", wildcardDomain, "privkey.pem")
 
-    if _, err := os.Stat(certFile); os.IsNotExist(err) {
-        return nil, fmt.Errorf("no certificate found for host: %s or wildcard domain: %s", clientHello.ServerName, wildcardDomain)
-    }
+	if _, err := os.Stat(certFile); os.IsNotExist(err) {
+		return nil, fmt.Errorf("no certificate found for host: %s or wildcard domain: %s", clientHello.ServerName, wildcardDomain)
+	}
 
-    err := r.LoadCertificate(wildcardDomain, certFile, keyFile)
-    if err != nil {
-        return nil, err
-    }
+	err := r.LoadCertificate(wildcardDomain, certFile, keyFile)
+	if err != nil {
+		return nil, err
+	}
 
-    r.mu.RLock()
-    defer r.mu.RUnlock()
-    return r.certs[wildcardDomain], nil
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.certs[wildcardDomain], nil
 }
 
 func (r *CertificateReloader) GetCertificate(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-    r.mu.RLock()
-    cert, ok := r.certs[clientHello.ServerName]
-    if !ok {
-        wildcardDomain := formatWildcardDomain(clientHello.ServerName)
-        if wildcardDomain != "" {
-            cert, ok = r.certs[wildcardDomain]
-        }
-    }
-    r.mu.RUnlock()
-    if ok {
-        return cert, nil
-    }
+	r.mu.RLock()
+	cert, ok := r.certs[clientHello.ServerName]
+	if !ok {
+		wildcardDomain := formatWildcardDomain(clientHello.ServerName)
+		if wildcardDomain != "" {
+			cert, ok = r.certs[wildcardDomain]
+		}
+	}
+	r.mu.RUnlock()
+	if ok {
+		return cert, nil
+	}
 
-    certFile := filepath.Join("certs", clientHello.ServerName, "fullchain.pem")
-    keyFile := filepath.Join("certs", clientHello.ServerName, "privkey.pem")
+	certFile := filepath.Join("certs", clientHello.ServerName, "fullchain.pem")
+	keyFile := filepath.Join("certs", clientHello.ServerName, "privkey.pem")
 
-    if _, err := os.Stat(certFile); os.IsNotExist(err) {
-        return r.getWildcardCertificate(clientHello)
-    }
+	if _, err := os.Stat(certFile); os.IsNotExist(err) {
+		return r.getWildcardCertificate(clientHello)
+	}
 
-    err := r.LoadCertificate(clientHello.ServerName, certFile, keyFile)
-    if err != nil {
-        return nil, err
-    }
+	err := r.LoadCertificate(clientHello.ServerName, certFile, keyFile)
+	if err != nil {
+		return nil, err
+	}
 
-    r.mu.RLock()
-    defer r.mu.RUnlock()
-    return r.certs[clientHello.ServerName], nil
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.certs[clientHello.ServerName], nil
 }
-
 
 func (r *CertificateReloader) LoadCertificate(hostname string, certFile, keyFile string) error {
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
@@ -227,14 +147,79 @@ func (r *CertificateReloader) ClearCache() {
 	r.certs = make(map[string]*tls.Certificate)
 }
 
+func executeCGI(scriptPath string, env []string, stdin io.Reader, w http.ResponseWriter) error {
+	cmd := exec.Command(scriptPath)
+	cmd.Env = env
+	cmd.Stdin = stdin
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	// Copy script output to the response writer
+	if _, err := io.Copy(w, stdout); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		http.Error(w, stderr.String(), http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
+}
+
+func cgiHandler(w http.ResponseWriter, r *http.Request) {
+	scriptPath := filepath.Clean("." + r.URL.Path)
+	info, err := os.Stat(scriptPath)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	if info.IsDir() {
+		scriptPath = filepath.Join(scriptPath, "index")
+		if _, err := os.Stat(scriptPath); err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+	}
+
+	env := os.Environ()
+	env = append(env,
+		"REQUEST_METHOD="+r.Method,
+		"SCRIPT_NAME="+r.URL.Path,
+		"QUERY_STRING="+r.URL.RawQuery,
+		"CONTENT_TYPE="+r.Header.Get("Content-Type"),
+		"CONTENT_LENGTH="+r.Header.Get("Content-Length"),
+	)
+
+	err = executeCGI(scriptPath, env, r.Body, w)
+	if err != nil {
+		log.Printf("Error executing CGI script: %v", err)
+	}
+}
+
+// aren't there a bunch more headers that need to
+// be sent as part of cgi spec?
+
 func main() {
 	reloader := &CertificateReloader{}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, HTTPS!")
-	})
+	http.HandleFunc("/", cgiHandler)
 
-	http.HandleFunc("/clear-cache", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/__clearcache", func(w http.ResponseWriter, r *http.Request) {
 		reloader.ClearCache()
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Certificate cache cleared")
@@ -250,31 +235,5 @@ func main() {
 	log.Println("Starting HTTPS server on :443")
 	log.Fatal(server.ListenAndServeTLS("", "")) // Certificates are loaded dynamically, based on the hostname
 }
-```
-
-### Key Changes:
-
-1. **Dynamic Certificate Loading**: 
-    - Modified `GetCertificate` to check if the certificate is in the cache. If not, it tries to load it from the `./certs/{hostname}/` directory.
-    - Checks if the cert file exists before attempting to load it.
-
-2. **Certificate Cache Clearing**:
-    - Added a `ClearCache` method to clear the cache.
-    - Added an HTTP endpoint `/clear-cache` to allow manual clearing of the certificate cache.
-
-This setup assumes that the certificate files are named `privkey
-
-
-
-
-
-
-ok give me updates to this code so it handles cgi
-basically when it gets a request
-it tries to find a script with that name
-and executes it
-if the name is a directory, find a file called "index"
-in that directory and execute it
-the config for the root directory is just an environment variable
 
 
